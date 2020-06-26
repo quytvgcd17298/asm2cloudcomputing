10fda8257a19318d6ee5f6f16dd9aa4dd7745e84
quytvgcd17298 <59139851+quytvgcd17298@users.noreply.github.com>
2020-06-26 21:26:23 +0700

    Update UpdateData.php

--- UpdateData.php
+++ UpdateData.php
@@ -41,7 +41,7 @@ if (empty(getenv("DATABASE_URL"))){
 
         // return the number of row affected
         //return $stmt->rowCount();
-$sql = "UPDATE student SET fname = 'Lee Chan Do' WHERE stuid = 'SV02'";
+$sql = "UPDATE product SET productname = 'ATN Toy' WHERE productid = '001'";
       $stmt = $pdo->prepare($sql);
 if($stmt->execute() == TRUE){
     echo "Record updated successfully.";

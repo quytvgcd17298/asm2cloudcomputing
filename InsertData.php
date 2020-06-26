07c541ddd55b6ae7623a60e08a3f38252b421fd3
quytvgcd17298 <59139851+quytvgcd17298@users.noreply.github.com>
2020-06-26 22:30:47 +0700

    Update InsertData.php

--- InsertData.php
+++ InsertData.php
@@ -55,7 +55,7 @@ if($pdo === false){
 //$stmt->bindParam(':class', 'GCD018');
 //$stmt->execute();
 //$sql = "INSERT INTO student(stuid, fname, email, classname) VALUES('SV02', 'Hong Thanh','thanhh@fpt.edu.vn','GCD018')";
-$sql = "INSERT INTO student(size, productid, productname, basicprice, residual)"
+$sql = "INSERT INTO product(size, productid, productname, basicprice, residual)"
         . " VALUES('$_POST[size]','$_POST[productid]','$_POST[productname]','$_POST[basicprice]','$_POST[residual]')";
 $stmt = $pdo->prepare($sql);
 //$stmt->execute();
